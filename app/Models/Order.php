950945d30cd28a518f5dbd29b6f267377b343634
kanwartalha009 <kanwartalha009@gmail.com>
2023-11-13 12:33:33 +0500

    a

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+    public function has_items(){
+        return $this->hasMany('App\Models\LineItem', 'order_id', 'id');
+    }
 }
