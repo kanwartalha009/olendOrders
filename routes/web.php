c140491cb1c74a5eca89f69d07f531b48b90737e
kanwartalha009 <kanwartalha009@gmail.com>
2023-11-13 11:53:55 +0500

    a

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,4 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-Route::any('/', [\App\Http\Controllers\OrderController::class, 'index'])->middleware(['verify.shopify'])->name('home');
+Route::any('/', [\App\Http\Controllers\OrderController::class, 'index'])->middleware(['auth.shopify'])->name('home');
