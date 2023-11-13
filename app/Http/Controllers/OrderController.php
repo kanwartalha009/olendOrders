b4c1517d166c817bc951ec82d97d3d40903da168
kanwartalha009 <kanwartalha009@gmail.com>
2023-11-13 12:03:11 +0500

    a

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -1,14 +1,19 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\User;
 use Illuminate\Http\Request;
+
 class OrderController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $shop = User::first();
         $response = $shop->api()->rest('GET', '/admin/api/2022-10/orders.json', ["status" => "any", "limit" => 250]);
         $orders = json_decode(json_encode($response));
-        dd($orders);
+        foreach ($orders->body->orders as $order) {
+            dd($order);
+        }
     }
 }
