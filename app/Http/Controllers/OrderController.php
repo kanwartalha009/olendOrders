1b977b4f7eed6b172b5298ad0b542ace2dee2c85
kanwartalha009 <kanwartalha009@gmail.com>
2023-11-14 12:48:30 +0500

    a

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -117,6 +117,7 @@ class OrderController extends Controller
             $response = $shop->api()->rest('GET', '/admin/api/2022-10/orders.json', ["limit" => 250, "status" => 'any']);
         }
         $orders = json_decode(json_encode($response));
+        dd($orders);
         orderJob::dispatchAfterResponse($orders);
         if (isset($orders->link->next)) {
             $this->ordersSync($orders->link->next);
